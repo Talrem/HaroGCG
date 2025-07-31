ce0e825a580b2ed9bbe47787184200047b7ac106
Talrem <david.merlat28@gmail.com>
2025-08-01 01:57:44 +0200

    Update to cards and commands

--- commands/utility/card-info.js
+++ commands/utility/card-info.js
@@ -11,8 +11,30 @@ module.exports = {
 				.setRequired(true),
 		),
 	async execute(interaction) {
-		let toSend = "Couldn't find the card.";
-		toSend = cards[interaction.options.getString('card-number').toUpperCase()].Name;
+		let toSend = "";
+		for(i = 0; i < cards[-1].amount ; i++){
+			if(cards[i].ID == interaction.options.getString('card-number').toUpperCase() || cards[i].Name.toUpperCase() == interaction.options.getString('card-number').toUpperCase()){
+				if(toSend != "") toSend += "\n";
+				toSend += "```Name: "+cards[i].Name;
+				toSend += "\nColor: " +cards[i].Color + " | Type: " +cards[i].Type;
+				if(cards[i].Color != "Token") toSend += "\nLV: " +cards[i].LV + " | Cost: " +cards[i].Cost;
+				if(cards[i].Type != "Command") toSend += "\nAffiliation: " +cards[i].Affiliation;
+				toSend += "\nEffect: " +cards[i].Effect;
+				switch(cards[i].Type){
+					case "Pilot": toSend += "\nPilot AP: "+cards[i].PilotAP+" | Pilot HP: "+cards[i].PilotHP+"\nPilot Effect: "+cards[i].PilotEffect;
+					break;
+					case "Unit": toSend += "\nAP: "+cards[i].AP+" | HP: "+cards[i].HP;
+					break;
+					case "Command-Pilot": toSend += "\nPilot Name: "+cards[i].PilotName+" | Pilot AP: "+cards[i].PilotAP+" | Pilot HP: "+cards[i].PilotHP;
+					break;
+					case "Base":toSend += "\nHP: "+cards[i].BaseHP;
+					break;
+					default:;
+				}
+				toSend +="```";
+			}
+		}
+		if(toSend == "") toSend = "Couln't find the card.";
 		const err = await interaction.reply(toSend);
 	},
 };
