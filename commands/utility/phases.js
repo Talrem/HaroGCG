9ad644268522229c29b44b88b89510378d242aac
Talrem <david.merlat28@gmail.com>
2025-08-14 00:39:27 +0200

    edit phases

--- commands/utility/phases.js
+++ commands/utility/phases.js
@@ -2,11 +2,19 @@ const { SlashCommandBuilder } = require('discord.js');
 const fs = require("fs");
 const decks = require("../../jsons/decks.json");
 
-function drawCard(array, amount){
-	let cards = new Array()
+function mulligan(deck, hand){
+
 }
 
-function paperRockScisors(array, channel){
+function drawCard(deck, amount) {
+	let cards = new Array()
+	for (i = 0; i < amount; i++) {
+		cards[i] = deck.pull();
+	}
+	return cards;
+}
+
+function paperRockScisors(array, channel) {
 	let order;
 	//send direct message to both players.
 	//wait for reaction from both players before assigning a winner.
@@ -14,8 +22,8 @@ function paperRockScisors(array, channel){
 	return array;
 }
 
-function initiateArrays(array, nbJ){
-	for(i = 0; i < nbJ ; i++){
+function initiateArray(array, nbJ) {
+	for (i = 0; i < nbJ; i++) {
 		array[i] = new Array();
 	}
 	return array;
@@ -33,33 +41,39 @@ module.exports = {
 	async execute(interaction) {
 
 
-let step = -1;
-let playerNumber = 2; // between 2 and 4;
-playerNumber = interaction.options.getString('nb-players');
-let turnPlayer;
-let decksToUse = new Array(playerNumber);
-let hands = new Array(playerNumber);
-let validTargets = new Array(playerNumber);
-let UnitsInPlay = new Array(playerNumber);
-let UnitsThatCanAttack = new Array(playerNumber);
-hands = initiateArray(hands);
-validTargets = initiateArray(validTargets);
-let resources = new Array();
-initiateArray(validTargets);
-let playerOrder = new Array();
-playerOrder = paperRockScisors(playerOrder);
-switch(step){
-case -1://Draw 5 cards, Mulligan?
-console.log("Start Phase");
-case 0://Activate all of your rested cards, gain 1 Resource card.
-console.log("Resource phase");
-case 1://The turn player draws one card from the top of their deck.
-console.log("Draw Phase");
-case 2://The turn player can play Bases, Units, Pilots, Commands.
-	//Both players can play <Action> Command cards.
-	//The turn player can declare an attack on : A Shield zone (Base => Shields), a Rested Enemy Unit
-console.log("Main Phase");
-case 3://Both players can play <Action> Command cards.
-console.log("End Phase");
-step = 0;
-}
+		let step = -1;
+		let playerNumber = 2; // between 2 and 4;
+		playerNumber = interaction.options.getString('nb-players');
+		let turnPlayer;
+		let decksToUse = new Array(playerNumber);
+		let hands = new Array(playerNumber);
+		let validTargets = new Array(playerNumber);
+		let UnitsInPlay = new Array(playerNumber);
+		let UnitsThatCanAttack = new Array(playerNumber);
+		hands = initiateArray(hands);
+		validTargets = initiateArray(validTargets);
+		let resources = new Array();
+		initiateArray(validTargets);
+		let playerOrder = new Array();
+		playerOrder = paperRockScisors(playerOrder);
+		switch (step) {
+			case -1:
+				//Every player draws 5 cards. They can Mulligan once if they want.
+				console.log("Start Phase");
+			case 0:
+				//Activate all of your rested cards, gain 1 Resource card.
+				console.log("Resource phase");
+			case 1:
+				//The turn player draws one card from the top of their deck.
+				console.log("Draw Phase");
+			case 2:
+				//The turn player can play Bases, Units, Pilots, Commands.
+				//Both players can play <Action> Command cards.
+				//The turn player can declare an attack on : A Shield zone (Base => Shields), a Rested Enemy Unit
+				console.log("Main Phase");
+			case 3://Both players can play <Action> Command cards.
+				console.log("End Phase");
+				step = 0;
+		}
+	},
+};
